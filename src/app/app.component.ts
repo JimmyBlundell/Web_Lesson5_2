be47dc42facaac34e81a533fff42d9695aab5f4f
Jimmy Blundell <jblundell123@gmail.com>
2020-09-30 23:47:48 -0500

    Functionality for ICP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  header = 'My 2020 To Do List';
+
   // define list of items
-  items= [];
+  public items: Array<{details: string, isComplete: boolean }> = [];
+
+  public newItem;
 
   // Write code to push new item
   submitNewItem() {
-
+    if (this.newItem.details === '') {
+      // do nothing
+    } else {
+      this.items.push({
+        details: this.newItem,
+        isComplete: false
+      });
+    }
   }
 
   // Write code to complete item
-  completeItem() {
-
+  completeItem(item) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].details === item.details) {
+        this.items[i].isComplete = true;
+      }
+    }
   }
 
   // Write code to delete item
-  deleteItem() {
-
+  deleteItem(index) {
+    this.items.splice(index, 1);
   }
 
+  toggleItem(index) {
+    const item = this.items[index];
+    if (item) {
+      item.isComplete = !item.isComplete;
+    }
+  }
 }
